2554168582ce8aacb1bc514543526d5494e50d1b
Roberto Serra <obi.serra@gmail.com>
2020-12-01 08:48:14 +0100

    math exp kata

---
+++ codewars/src/codewars/evaluate_math_exp.clj
@@ -0,0 +1,25 @@
+(ns codewars.evaluate_math_exp)
+
+(def ops '("+" "-" "*" "/"))
+
+(defn tokenize []
+
+)
+
+(defn calc [expression]
+
+
+  0
+)
+
+
+(comment 
+  ;;2.0
+  (calc "1+1") 
+
+
+  (re-find #"\+" "1+1")
+
+;;
+
+  )
